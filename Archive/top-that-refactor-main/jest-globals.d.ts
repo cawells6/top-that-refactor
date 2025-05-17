3d3d059ab1e29c25b1f900fd928f111811245eab
Christopher Wells <cawells6@dmacc.edu>
2025-05-16 20:49:38 -0500

    trying to understand ts errors

--- Archive/top-that-refactor-main/jest-globals.d.ts
+++
@@ -1,9 +0,0 @@
-// jest-globals.d.ts
-// This file tells TypeScript about Jest's global functions and other globals
-
-declare global {
-  // Socket.IO client
-  function io(url?: string | object, options?: object): any;
-}
-
-export {}; // This ensures this file is treated as a module
